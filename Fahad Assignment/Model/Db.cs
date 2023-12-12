9fa5cc66cc3509ec493ae3f5967243f5aaab7c95
Fahadali-Ali <20101001-045@uskt.edu.pk>
2023-12-12 22:42:53 +0500

    .

---
+++ Fahad Assignment/Model/Db.cs
@@ -0,0 +1,16 @@
+﻿using assignement.data;
+using Microsoft.EntityFrameworkCore;
+
+namespace assignement.Models
+{
+    public class Db : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Faculty> Faculty { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Enrolled> Enrolled { get; set; }
+     
+        }
+
+    }
+}
